100a83562468bf667a780d634ac1e5ca281ae9c5
tiviku69 <tiviku69@gmail.com>
2025-06-29 20:25:39 +0700

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,36 @@
+let scrollmenu = document.createElement('div');
+scrollmenu.align ="center";
+scrollmenu.className = "scrollmenu";
+document.body.appendChild(scrollmenu);
+
+let a1 = document.createElement('span');
+a1.innerHTML = "<a class='film' href='m3up/1.html '><button><img class='gfilm' src=' https://raw.githubusercontent.com/tiviku69/apk/refs/heads/main/g.jpg ' alt=' eps.1 '></button></a>";
+scrollmenu.appendChild(a1);
+
+let a2 = document.createElement('span');
+a2.innerHTML = "<a class='film' href=' vod/2.Avatar.The.Legend.Of.Aang.(2005).dubbing.html '><button><img class='gfilm' src=' https://raw.githubusercontent.com/tiviku69/apk/refs/heads/main/tiviku.png ' alt=' eps.2 '></button></a>";
+scrollmenu.appendChild(a2);
+
+let a3 = document.createElement('span');
+a3.innerHTML = "<a class='film' href=' vod/3.Avatar.The.Legend.Of.Aang.(2005).dubbing.html '><button><img class='gfilm' src=' https://raw.githubusercontent.com/tiviku69/apk/refs/heads/main/tiviku.png ' alt=' eps.3 '></button></a>";
+scrollmenu.appendChild(a3);
+
+let a4 = document.createElement('span');
+a4.innerHTML = "<a class='film' href=' vod/4.Avatar.The.Legend.Of.Aang.(2005).dubbing.html '><button><img class='gfilm' src=' https://raw.githubusercontent.com/tiviku69/apk/refs/heads/main/tiviku.png ' alt=' eps.4 '></button></a>";
+scrollmenu.appendChild(a4);
+
+let a5 = document.createElement('span');
+a5.innerHTML = "<a class='film' href=' vod/5.Avatar.The.Legend.Of.Aang.(2005).dubbing.html '><button><img class='gfilm' src=' https://raw.githubusercontent.com/tiviku69/apk/refs/heads/main/tiviku.png ' alt=' eps.5 '></button></a>";
+scrollmenu.appendChild(a5);
+
+let a6 = document.createElement('span');
+a6.innerHTML = "<a class='film' href=' vod/6.Avatar.The.Legend.Of.Aang.(2005).dubbing.html '><button><img class='gfilm' src=' https://raw.githubusercontent.com/tiviku69/apk/refs/heads/main/tiviku.png ' alt=' eps.6 '></button></a>";
+scrollmenu.appendChild(a6);
+
+let a7 = document.createElement('span');
+a7.innerHTML = "<a class='film' href=' vod/7.Avatar.The.Legend.Of.Aang.(2005).dubbing.html '><button><img class='gfilm' src=' https://raw.githubusercontent.com/tiviku69/apk/refs/heads/main/tiviku.png ' alt=' eps.7 '></button></a>";
+scrollmenu.appendChild(a7);
+
+let a8 = document.createElement('span');
+a8.innerHTML = "<a class='film' href=' vod/8.Avatar.The.Legend.Of.Aang.(2005).dubbing.html '><button><img class='gfilm' src=' https://raw.githubusercontent.com/tiviku69/apk/refs/heads/main/tiviku.png ' alt=' eps.8 '></button></a>";
+scrollmenu.appendChild(a8);
