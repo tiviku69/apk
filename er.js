bc46cc018a0ef722b44b22d7f4d5f623d96a8e46
tiviku69 <tiviku69@gmail.com>
2025-07-19 19:09:36 +0700

    Add files via upload

--- er.js
+++ er.js
@@ -1,17 +1,55 @@
-const images = document.querySelectorAll('.image');
-let currentIndex = 0;
+const files = [ 'captain.json','cmpr.json' ]; // Array file JSON
 
-function updateImagePosition() {
-    const shift = currentIndex * -170; // Adjust based on image width + margin
-    document.querySelector('.image-container').style.transform = `translateX(${shift}px)`;
+files.forEach(file => {
+               fetch(file)
+                   .then(response => response.json())
+                   .then(data => {
+                       data.forEach(item => {
+
+const container = document.getElementById('container');
+                          
+const img = document.createElement('img');
+img.id = 'imgv';
+img.src = item.logo;
+
+const pp = document.createElement('p');
+pp.className = 're';
+pp.innerText = item.ttl;
+
+const dv = document.createElement('div');
+dv.className = 'responsive-div';
+dv.onclick = () => playVideo(item.lnk);
+
+function playVideo(videoFile) {
+            window.location.href = `ply.html?video=${videoFile}`;
+        }
+
+dv.appendChild(img);
+
+dv.appendChild(pp);
+
+container.appendChild(dv);
+                                                   
+                       });
+                   })
+                   
+                   .catch(error => console.error('Error loading JSON:', error));
+           });
+
+function prosesMenu() {
+    var input = document.getElementById("cari");
+    var filter = input.value.toLowerCase();
+    var li = document.querySelectorAll('.responsive-div');
+
+    for (var i = 0; i < li.length; i++) {
+        // Menggunakan innerHTML dari button
+        if (li[i].innerHTML.toLowerCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
 }
 
-document.addEventListener('keydown', (event) => {
-    if (event.key === 'ArrowRight') {
-        currentIndex = (currentIndex + 1) % images.length;
-        updateImagePosition();
-    } else if (event.key === 'ArrowLeft') {
-        currentIndex = (currentIndex - 1 + images.length) % images.length;
-        updateImagePosition();
-    }
-});
+// Tambahkan event listener untuk memanggil prosesMenu saat input berubah
+document.getElementById("cari").addEventListener("input", prosesMenu);
