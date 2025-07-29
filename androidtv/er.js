d7093443706b6e43dd12d15fb66f819607a3f208
tiviku69 <tiviku69@gmail.com>
2025-07-29 12:59:49 +0700

    Add files via upload

--- androidtv/er.js
+++ androidtv/er.js
@@ -1,4 +1,4 @@
-const files = [ 'captain.json','cmpr.json','ghost.json' ]; // Array file JSON
+const files = [ 'captain.json','cmpr.json','ghost.json','avatar.json' ]; // Array file JSON
 
 files.forEach(file => {
                fetch(file)
@@ -29,7 +29,7 @@ function playVideo(videoFile, logoFile, textFile) {
     sessionStorage.setItem('videoFile', videoFile);
     sessionStorage.setItem('logoFile', logoFile);
     sessionStorage.setItem('textFile', textFile);
-    window.location.href = 'ply.html';
+    window.location.href = 'play.html';
 }
 
 dv.appendChild(img);
---
+++ androidtv/er2.js
@@ -0,0 +1,113 @@
+// Mendapatkan parameter dari URL
+        const params = new URLSearchParams(window.location.search);
+        const videoUrl = params.get('video');
+        
+window.onload = function() {
+            const videoFile = sessionStorage.getItem('videoFile');
+            const logoFile = sessionStorage.getItem('logoFile');
+            const textFile = sessionStorage.getItem('textFile');
+            
+            const videoPlayer = document.getElementById('videoPlayer');
+            const videoSource = document.getElementById('videoSource');
+            const jdl = document.getElementById('jdl');
+            const imgg = document.getElementById('imgg');
+            
+if (videoFile) {
+
+videoSource.src = videoFile;
+
+videoPlayer.setAttribute('poster', logoFile);
+
+jdl.innerText =  textFile;
+
+imgg.src = logoFile;
+
+
+                videoPlayer.load();
+            }
+        };
+        
+document.addEventListener('keydown', (event) => {
+        if (event.key === 'ArrowDown') {
+            controls.style.display = 'block';
+        }
+        if (event.key === 'ArrowUp') {
+            controls.style.display = 'none';
+        }
+    });
+        
+// Add event listeners to buttons
+        const video = document.querySelector(".video");
+const toggleButton = document.querySelector(".toggleButton");
+const progress = document.querySelector(".progress");
+const progressBar = document.querySelector(".progress__filled");
+const sliders = document.querySelectorAll(".controls__slider");
+const skipBtns = document.querySelectorAll("[data-skip]");
+
+function togglePlay() {
+  if (video.paused || video.ended) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+function updateToggleButton() {
+  toggleButton.innerHTML = video.paused ? "<b>▶</b>" : "❚ ❚";
+}
+
+function handleProgress() {
+  const progressPercentage = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${progressPercentage}%`;
+}
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
+function handleSliderUpdate() {
+  video[this.name] = this.value;
+}
+function handleSkip() {
+  video.currentTime += +this.dataset.skip;
+}
+toggleButton.addEventListener("click", togglePlay);
+video.addEventListener("click", togglePlay);
+video.addEventListener("play", updateToggleButton);
+video.addEventListener("pause", updateToggleButton);
+video.addEventListener("timeupdate", handleProgress);
+sliders.forEach((slider) => {
+  slider.addEventListener("change", handleSliderUpdate);
+});
+skipBtns.forEach((btn) => {
+  btn.addEventListener("click", handleSkip);
+});
+
+sliders.forEach((slider) => {
+  slider.addEventListener("change", handleSliderUpdate);
+});
+document.addEventListener("keydown", (e) => {
+  if (e.code === "Space") togglePlay();
+});
+
+const videoPlayer = document.getElementById('videoPlayer');
+    const coDiv = document.getElementById('co');
+    const imgg = document.getElementById('imgg');
+    const jdl = document.getElementById('jdl');
+
+    videoPlayer.addEventListener('play', function() {
+      coDiv.style.backgroundColor = 'rgb(0,0,0,0.0)';
+      imgg.style.display = 'none';
+      jdl.style.display = 'none'; // Sembunyikan div id "co"
+    });
+
+    videoPlayer.addEventListener('pause', function() {
+      coDiv.style.backgroundColor = 'rgb(0,0,0,0.5)';
+      imgg.style.display = 'block';
+      jdl.style.display = 'block'; // Tampilkan kembali ketika video dipause
+    });
+
+    videoPlayer.addEventListener('ended', function() {
+      coDiv.style.backgroundColor = 'rgb(0,0,0,0.5)';
+       imgg.style.display = 'block';
+       jdl.style.display = 'block';// Tampilkan kembali setelah video selesai
+    });
