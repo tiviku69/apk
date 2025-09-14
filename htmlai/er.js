5287345c764279ae19ec7d37308882aef603c2a7
tiviku69 <tiviku69@gmail.com>
2025-09-14 09:25:59 +0700

    Add files via upload

--- htmlai/er.js
+++ htmlai/er.js
@@ -1,6 +1,4 @@
-const atas = document.getElementById('atas');
-atas.innerHTML = '<h1>tiviku</h1> <b>by tiviku</b> <input type="text" name="" id="cari" onkeyup="prosesMenu()" placeholder="cari..."> ';
-
+const container = document.getElementById('container');
 const files = [ 'https://raw.githubusercontent.com/tiviku69/apk/main/cmpr.json','https://raw.githubusercontent.com/tiviku69/apk/main/captain.json','https://raw.githubusercontent.com/tiviku69/apk/main/avat.json','https://raw.githubusercontent.com/tiviku69/apk/main/ghost.json','https://raw.githubusercontent.com/tiviku69/apk/main/avatar.json','https://raw.githubusercontent.com/tiviku69/apk/main/squid.json','https://raw.githubusercontent.com/tiviku69/apk/main/journey.json','https://raw.githubusercontent.com/tiviku69/apk/main/one.json','https://raw.githubusercontent.com/tiviku69/apk/main/mp4.json' ];
 
 files.forEach(file => {
@@ -8,7 +6,10 @@ files.forEach(file => {
         .then(response => response.json())
         .then(data => {
             data.forEach(item => {
-                const container = document.getElementById('container');
+                const dv = document.createElement('div');
+                dv.className = 'responsive-div';
+                dv.onclick = () => playVideo(item.lnk, item.logo, item.ttl);
+
                 const img = document.createElement('img');
                 img.id = 'imgv';
                 img.src = item.logo;
@@ -21,13 +22,9 @@ files.forEach(file => {
                 dur.className = 'dur';
                 dur.innerText = item.dur;
 
-                const dv = document.createElement('div');
-                dv.className = 'responsive-div';
-                dv.onclick = () => playVideo(item.lnk, item.logo, item.ttl);
-
                 dv.appendChild(img);
-                dv.appendChild(pp);
                 dv.appendChild(dur);
+                dv.appendChild(pp);
                 container.appendChild(dv);
             });
         })
@@ -46,12 +43,11 @@ function prosesMenu() {
     var filter = input.value.toLowerCase();
     var li = document.querySelectorAll('.responsive-div');
     for (var i = 0; i < li.length; i++) {
-        if (li[i].innerHTML.toLowerCase().indexOf(filter) > -1) {
+        var videoTitle = li[i].querySelector('.re').innerText.toLowerCase();
+        if (videoTitle.indexOf(filter) > -1) {
             li[i].style.display = "";
         } else {
             li[i].style.display = "none";
         }
     }
 }
-
-document.getElementById("cari").addEventListener("input", prosesMenu);
