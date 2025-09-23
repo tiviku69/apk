4ad10d3c74dbad557e3286138f485219c52fbf02
tiviku69 <tiviku69@gmail.com>
2025-09-24 06:36:08 +0700

    Add files via upload

--- htmlai/new.js
+++ htmlai/new.js
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const hours = String(now.getHours()).padStart(2, '0');
         const minutes = String(now.getMinutes()).padStart(2, '0');
         const seconds = String(now.getSeconds()).padStart(2, '0');
-        digitalClock.textContent = `${hours}:${minutes}:${seconds}`;
+        digitalClock.textContent = `${hours}:${minutes}`;
     }
 
     setInterval(updateClock, 1000);
